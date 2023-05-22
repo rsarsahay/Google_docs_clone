39381ade80399d7759d298b8783c0b090c31277e
Kumari Ritu Sarsahay <102847337+rsarsahay@users.noreply.github.com>
2023-05-22 16:00:16 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,28 +12,6 @@ import "firebase/compat/firestore";
 };
 
 
-/*const firebaseConfig = {
-  apiKey: "AIzaSyACwT0iUWcWXh2funvQhF3BW5ut8HcSR5U",
-  authDomain: "docs-clone-2fd36.firebaseapp.com",
-  projectId: "docs-clone-2fd36",
-  storageBucket: "docs-clone-2fd36.appspot.com",
-  messagingSenderId: "379543444035",
-  appId: "1:379543444035:web:bd9102e0806c816b0192f6",
-  measurementId: "G-0FTHT5JQ9R"
-};*/
-
-
-
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDFxFGdHl9obp9j_eIYebG2Ywd_QKp0rgE",
-  authDomain: "next--docs-719cd.firebaseapp.com",
-  projectId: "next--docs-719cd",
-  storageBucket: "next--docs-719cd.appspot.com",
-  messagingSenderId: "725211284405",
-  appId: "1:725211284405:web:7ddb1da1664a79228ccffc",
-  measurementId: "G-B9PNJTTN4T",
-};*/
-
 // checking whether the app is already initialize or not
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
